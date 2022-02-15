4b153cb8c515b34f15202d64be0b66827cb4e695
Dheeyi William <dheeyi@gmail.com>
2022-02-15 15:06:16 -0400

    Adding subForm directory

--- dynaform-editor/src/examples/sub-form/index.ts
+++ dynaform-editor/src/examples/sub-form/index.ts
@@ -3,13 +3,14 @@ import uischema from './uischema.json';
 import data from './data.json';
 import { JsonSchema, UISchemaElement } from '@jsonforms/core';
 
-import subFormSchema from './subForm.json';
+import subFormSchema from './subForm/subForm.json';
+import subFormSchemaUI from './subForm/subformuischema.json';
 import $RefParser from '@apidevtools/json-schema-ref-parser';
 
-const yourRemoteSchemaResolver = {
+const schemaResolver = {
   order: 1,
   canRead: function (file: { url: string | string[] }) {
-    return file.url.indexOf('subForm.json') !== -1;
+    return file.url.indexOf('subForm/subForm.json') !== -1;
   },
   read: function () {
     return JSON.stringify(subFormSchema);
@@ -20,17 +21,40 @@ const refParserOptions = {
     circular: false,
   },
   resolve: {
-    definitions: yourRemoteSchemaResolver,
+    definitions: schemaResolver,
   },
 };
 
-const getSchema = async (): Promise<any> =>
-  await $RefParser.dereference(schema, refParserOptions);
+const schemaUIResolver = {
+  order: 1,
+  canRead: function (file: { url: string | string[] }) {
+    return file.url.indexOf('subForm/subformuischema.json') !== -1;
+  },
+  read: function () {
+    return JSON.stringify(subFormSchemaUI);
+  },
+};
+const refUIParserOptions = {
+  dereference: {
+    circular: false,
+  },
+  resolve: {
+    definitions: schemaUIResolver,
+  },
+};
+
+const getSchema = async (): Promise<any> => {
+  const [newSchema] = await Promise.all([
+    $RefParser.dereference(schema, refParserOptions),
+    $RefParser.dereference(uischema, refUIParserOptions),
+  ]);
+  return newSchema;
+};
 
 const newSchema: any = getSchema().then((response) => response);
 
 export const input: {
-  newSchema: JsonSchema;
+  schema: JsonSchema;
   uischema: UISchemaElement;
   data: any;
 } = { newSchema, uischema, data };
