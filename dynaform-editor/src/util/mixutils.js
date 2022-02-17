a60089d675b8504f3456525b7a9a042136b19d24
henry jordan <jonathan.quispe@processmaker.com>
2022-02-17 10:13:20 -0400

    update braces

--- dynaform-editor/src/util/mixutils.js
+++ dynaform-editor/src/util/mixutils.js
@@ -11,15 +11,13 @@ export let VariableBuilder = {
                 }
             }
         });
-        console.log('BUILD VARIABLES FINAL');
-        console.log(json);
         return json;
     },
     findVariables(json, j, key, str) {
-        let res = str.match(/@@[A-Za-z0-9]+/g);
+        let res = str.match(/{{\s*[A-Za-z0-9]+\s*}}/g);
         if (res) {
             res.forEach((el) => {
-                let r = el.replace('@@', '');
+                let r = el.replace('{{', '').replace('}}', '').trim();
                 j[key] = j[key].replace(el, json[r] || el);
             });
         }
