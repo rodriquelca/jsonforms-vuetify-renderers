0ff31634c446a0d26ceac643cd97cab803883a87
Fabio Guachalla <frguachallab@gmail.com>
2022-03-29 21:32:52 -0400

    Time properties
    set time

--- vue2-vuetify/src/controls/TimeControlRenderer.vue
+++ vue2-vuetify/src/controls/TimeControlRenderer.vue
@@ -5,23 +5,52 @@
     :isFocused="isFocused"
     :appliedOptions="appliedOptions"
   >
-    <v-text-field
-      type="time"
-      :id="control.id + '-input'"
-      :class="styles.control.input"
-      :disabled="!control.enabled"
-      :autofocus="appliedOptions.focus"
-      :placeholder="appliedOptions.placeholder"
-      :label="computedLabel"
-      :hint="control.description"
-      :persistent-hint="persistentHint()"
-      :required="control.required"
-      :error-messages="control.errors"
-      :value="control.data"
-      @change="onChange"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-    />
+    <v-menu
+      ref="menu"
+      v-model="menu"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        :return-value.sync="time"
+        transition="scale-transition"
+        offset-y
+        max-width="290px"
+        min-width="290px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          type="time"
+          v-model="dataTime"
+          append-icon="mdi-clock-time-four-outline"
+          :id="control.id + '-input'"
+          :class="styles.control.input"
+          :disabled="!control.enabled"
+          :autofocus="appliedOptions.focus"
+          :placeholder="appliedOptions.placeholder"
+          :label="computedLabel"
+          :hint="control.description"
+          :persistent-hint="persistentHint()"
+          :required="control.required"
+          :error-messages="control.errors"
+          @focus="isFocused = true"
+          @blur="isFocused = false"
+
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-time-picker
+        v-if="menu"
+        v-model="dataTime"
+        format="ampm"
+        @click:minute="$refs.menu.save(time)"
+        :min="control.schema.minDate"
+        :max="control.schema.maxDate"
+        no-title
+        scrollable
+      >
+      </v-time-picker>
+    </v-menu>
   </control-wrapper>
 </template>
 
@@ -39,20 +68,55 @@ import {
   RendererProps,
 } from '@jsonforms/vue2';
 import { default as ControlWrapper } from './ControlWrapper.vue';
-import { useVuetifyControl } from '../util';
-import { VTextField } from 'vuetify/lib';
+import { useVuetifyControl, parseDateTime } from '../util';
+import { VTextField, VMenu, VTimePicker, VBtn } from 'vuetify/lib';
 
 const controlRenderer = defineComponent({
   name: 'time-control-renderer',
   components: {
     ControlWrapper,
     VTextField,
+    VMenu,
+    VTimePicker,
+    VBtn,
   },
   props: {
     ...rendererProps<ControlElement>(),
   },
   setup(props: RendererProps<ControlElement>) {
-    return useVuetifyControl(useJsonFormsControl(props));
+    return {
+      ...useVuetifyControl(useJsonFormsControl(props)),
+      menu: false,
+      date: null,
+    };
+  },
+  computed: {
+    dataTime: {
+      get(): string | null | undefined {
+        const datetimeLocalFormat = 'HH:mm:ss';
+        const saveFormat = this.appliedOptions.dateTimeSaveFormat ?? undefined;
+        const value = this.control.data as string | undefined | null;
+
+        const dateTime = parseDateTime(value, saveFormat);
+        return dateTime ? dateTime.local().format(datetimeLocalFormat) : value;
+      },
+      set(value: string) {
+        debugger;
+        const datetimeLocalFormats = ['HH:mm:ss', 'hh:mm:ss', 'HH:mm'];
+        const saveFormat = this.appliedOptions.dateTimeSaveFormat ?? 'HH:mm:ss';
+
+        const dateTime = parseDateTime(value, datetimeLocalFormats);
+        const result = dateTime ? dateTime.format(saveFormat) : value;
+
+        this.onChange(result);
+      },
+    },
+  },
+  methods: {
+    clearDate() {
+      this.menu = false;
+      this.dataTime = null;
+    },
   },
 });
 
